ef407fa5d6b3d855b0c1a3e5a78cf9b6c9be6316
Eemeli Aro <eemeli@gmail.com>
2019-10-22 20:09:34 +0300

    Add "test" scripts

--- typescript/src/esm.ts
+++ typescript/src/esm.ts
@@ -1,5 +1,5 @@
 import Foo from 'foo'
-import React from 'react'
+import * as React from 'react'
 
 const Bar = React.createElement('div', null, React.createElement(Foo, null))
 console.log(Bar.props.children.type)
